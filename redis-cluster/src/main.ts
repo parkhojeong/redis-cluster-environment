2fcf49fc3478188f1afd204a4ebfd13dd3139a97
parkhojeong <parkhj062@gmail.com>
2023-11-21 21:04:45 +0900

    redis 클러스터 설정

--- redis-cluster/src/main.ts
+++ redis-cluster/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import Redis from "ioredis";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  const cluster = new Redis.Cluster([
+    {
+      port: 7001,
+      host: "127.0.0.1"
+    }
+  ]);
+
+  console.log(await cluster.get("c"));
+  await cluster.set("c", "123");
+
+
   await app.listen(3000);
 }
 bootstrap();
